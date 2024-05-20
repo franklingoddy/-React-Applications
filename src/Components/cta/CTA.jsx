ffbae54a1daf272aa902784d55dc5e2274994350
franklin <franklingoddy@gmail.com>
2024-05-20 20:12:32 +0100

    initial commit

---
+++ src/Components/cta/CTA.jsx
@@ -0,0 +1,21 @@
+
+import "./CTA.css"
+
+
+function CTA(){
+    return(
+        <>
+        <div className="gpt3__cta">
+            <div className="gpt3__cta-contents">
+                <p>Request Early Access to Get Started</p>
+                <h3>Register Today & Start Exploring the Innovative and Easy to Use Crypto Trading Platform. Built For Everyone.</h3>
+            </div>
+            <div className="gpt3__cta-btn">
+                <button type="button">Get Started</button>
+            </div>
+        </div>
+        </>
+    )
+}
+
+ export default CTA
